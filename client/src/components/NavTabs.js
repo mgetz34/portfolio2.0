d0f65c2e5fd9b6a7bc4292f5fa4f33486ef9ebd1
mgetz34 <mgetz340@gmail.com>
2023-01-10 18:16:28 -0700

    added skeleton code to all about, home, contact, and projects pages, added navtabs, index.js, and app.js skeleton, and imported all necessary pages and components to the PortfolioiContainer.js file

---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function NavTabs({ }) {
+    return (
+
+    )
+}
+
+export default NavTabs;
---
+++ client/src/components/PortfolioContainer.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+
+export default function PortfolioContainer() {
+
+}
