b5a3ba43241702d0a0b431eb4ac6410a9626897e
mgetz34 <mgetz340@gmail.com>
2023-01-11 09:47:27 -0700

    started converting files to utilize react reouter dom for page rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Projects from "./components/pages/Projects";
+
+
+//define the routes anbd import components 
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Projects from "./components/pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import PortfolioContainer from './components/PortfolioContainer'
 
-const App = () => <PortfolioContainer />;
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/projects" element={<Projects />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
+}
 
 export default App;
