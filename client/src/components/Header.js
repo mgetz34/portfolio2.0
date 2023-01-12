78706a13553e31c53adaddc6e95f8ee8d6e847c2
mgetz34 <mgetz340@gmail.com>
2023-01-12 16:09:05 -0700

    added boilerplate jsx for the header and footer.js files

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,18 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-function Header() {
+const Header = () => {
     return (
-        
-    )
-}
+        <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
+            <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
+                <Link className="text-dark" to="/">
+                    <h1 className="m-0" style={{ fontSize: '3rem' }}>
+                        Michael Getz
+                    </h1>
+                </Link>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
